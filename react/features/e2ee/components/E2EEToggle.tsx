a0c330a8661d18f3cd73ee24704abe5ce24944a4
Hishara Dilshan <hisharadilshan3@gmail.com>
2026-01-06 00:53:18 +0000

    chore: adding a debounce to toggle e2ee in conference page

--- react/features/e2ee/components/E2EEToggle.tsx
+++ react/features/e2ee/components/E2EEToggle.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from 'tss-react/mui';
@@ -35,6 +35,12 @@ const useStyles = makeStyles()(theme => {
             fontWeight: 600,
             color: theme.palette.text01,
             marginRight: '8px'
+        },
+        waitMessage: {
+            fontSize: '0.75rem',
+            color: theme.palette.text02,
+            fontStyle: 'italic',
+            marginLeft: '8px'
         }
     };
 });
@@ -48,6 +54,7 @@ const E2EEToggle = () => {
     const { classes } = useStyles();
     const { t } = useTranslation();
     const dispatch = useDispatch();
+    const [isProcessing, setIsProcessing] = useState(false);
 
     const e2eeEnabled = useSelector((state: IReduxState) => state['features/e2ee'].enabled);
     const maxMode = useSelector((state: IReduxState) => state['features/e2ee'].maxMode);
@@ -75,12 +82,26 @@ const E2EEToggle = () => {
             </span>
             <Switch
                 checked={e2eeEnabled}
-                disabled={!enabled}
+                disabled={!enabled || isProcessing}
                 onChange={(checked) => {
+                    if (isProcessing) {
+                        return;
+                    }
+                    
+                    setIsProcessing(true);
                     sendAnalytics(createE2EEEvent(`enabled.${String(checked)}`));
                     dispatch(toggleE2EE(checked || false));
+                    
+                    setTimeout(() => {
+                        setIsProcessing(false);
+                    }, 2000);
                 }}
             />
+            {isProcessing && (
+                <span className={classes.waitMessage}>
+                    Please wait...
+                </span>
+            )}
         </div>
     );
 };
